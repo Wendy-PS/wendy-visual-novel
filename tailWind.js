953dc029849f7ef2c843de87a667be7c700d02d4
Wendy-PS <endypereiradsilva100@gmail.com>
2025-10-03 14:38:19 -0300

    Move arquivos para a raiz do projeto

---
+++ tailWind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        doki: ['"Comic Neue"', 'sans-serif'],
+      },
+    },
+  },
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        honey: '#FEDC82',
+      },
+    },
+  },
+}
